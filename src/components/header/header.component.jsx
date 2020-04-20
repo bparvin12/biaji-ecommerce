e3699214a154d8bde11c791f384fa4785d8770a3
BijanParvin <bparvin12@gmail.com>
2020-04-20 11:34:06 -0700

    added state to toggle cartdropdown. carticon and cartdropdown. cart reducer

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,10 +3,12 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { ReactComponent as Logo } from '../../assets/original.svg';
 import { auth } from '../../firebase/firebase.utils';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 import './header.styles.scss';
 
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
     <div className='header'>
         <Link to='/' className='logo-container'>
             <Logo className='logo' />
@@ -23,12 +25,15 @@ const Header = ({ currentUser }) => (
                  <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
                  : <Link className='option' to='/signin'>SIGN IN</Link>
              }
+             <CartIcon />
         </div>
+        {hidden ? null : <CartDropdown />}
     </div>
 )
 
-const mapStateToProps = (state) => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({user: {currentUser}, cart: {hidden}}) => ({
+    currentUser, 
+    hidden
 })
 
 export default connect(mapStateToProps)(Header);
