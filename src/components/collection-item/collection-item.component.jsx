429c8dafb9ca7530e14373aa037b567414de679b
BijanParvin <bparvin12@gmail.com>
2020-04-20 12:08:33 -0700

    updated shop data. updated cart.utils

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,8 +1,13 @@
 import React from "react";
+import { connect } from 'react-redux';
+import { addItem } from '../../redux/cart/cart.actions';
+import CustomButton from '../custom-button/custom-button.component';
 import "./collection-item.styles.scss";
 
-const CollectionItem = ({ id, name, price, imageUrl }) => (
-    <div className="collection-item">
+const CollectionItem = ({ item, addItem }) => {
+    const {name, price, imageUrl} = item;
+    return (
+        <div className="collection-item">
         <div
             className="image"
             style={{
@@ -13,7 +18,18 @@ const CollectionItem = ({ id, name, price, imageUrl }) => (
             <span className="name">{name}</span>
             <span className="price">{price}</span>
         </div>
+        <CustomButton inverted onClick={() => addItem(item)}>
+            Add to Cart
+        </CustomButton>
     </div>
-);
+    )
+};
 
-export default CollectionItem;
+const mapDispatchToProps = dispatch => ({
+    addItem: item => dispatch(addItem(item))
+})
+
+export default connect(
+    null,
+    mapDispatchToProps
+)(CollectionItem);
--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,6 +1,8 @@
 import { CartActionTypes } from './cart.types'
+import { addItemToCart } from './cart.utils'
 const INITIAL_STATE = {
-    hidden: true
+    hidden: true,
+    cartItems: []
 }
 
 const cartReducer = (state = INITIAL_STATE, action) => {
@@ -9,8 +11,13 @@ const cartReducer = (state = INITIAL_STATE, action) => {
             return {
                 ...state, 
                 hidden: !state.hidden
-            }
-            default: 
+            };
+        case CartActionTypes.ADD_ITEM:
+            return {
+                ...state,
+                cartItems: addItemToCart(state.cartItems, action.payload)
+            };
+        default: 
             return state;
     }
 }
